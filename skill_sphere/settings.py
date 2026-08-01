5e8175128be18ff06af8586d2ca0f51cd080b7cb
Soyokaze00 <unikaze2004@example.com>
2026-08-01 13:01:45 +0330

    chore(settings): register django_celery_results app
    Needed for CELERY_RESULT_BACKEND = 'django-db' to store task results.

--- skill_sphere/settings.py
+++ skill_sphere/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
+    'django_celery_results',
     'tailwind',
     "django.contrib.humanize",
     'search', 
